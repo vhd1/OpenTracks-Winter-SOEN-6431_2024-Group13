63c742c55d82b437199d1bcb3519abab96d57742
Emeka Okechukwu <chuks.egkedu@gmail.com>
2024-04-03 16:09:26 -0400

    Add new screen for ski profile
    Implement a blank screen when the user clicks on the ski profile subsection. Related to issue #84

---
+++ src/main/java/de/dennisguse/opentracks/settings/UserProfileSettingsFragment.java
@@ -0,0 +1,13 @@
+package de.dennisguse.opentracks.settings;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class UserProfileSettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+
+    }
+}
