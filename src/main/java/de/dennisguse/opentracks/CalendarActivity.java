8ffae3147ed2ca1809f49dd79104be0754310828
Soham Sakaria <soham.s2411@gmail.com>
2024-04-03 21:06:15 -0400

    Add date variable

--- src/main/java/de/dennisguse/opentracks/CalendarActivity.java
+++ src/main/java/de/dennisguse/opentracks/CalendarActivity.java
@@ -1,13 +1,19 @@
 package de.dennisguse.opentracks;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CalendarView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import java.util.Date;
 
 
 public class CalendarActivity extends AppCompatActivity {
+
+    private Date selectedDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +22,25 @@ public class CalendarActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener((view) -> finish());
 
         Button dateSelectButton = findViewById(R.id.calendar_date_selected_activity);
-        dateSelectButton.setOnClickListener((view)->{
-            // add selected date logic.
-        });
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+            calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                @Override
+                public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                    // Display the selected date
+                    setSelectedDate(new Date(year-1900, month, dayOfMonth));
+                }
+            });
+            dateSelectButton.setOnClickListener((view)->{
+                // Navigate to a new screen with passing the date;
+            });
     }
 
+
+    public void setSelectedDate(Date date){
+        this.selectedDate = date;
+    }
+
+    public Date getSelectedDate(){
+        return selectedDate;
+    }
 }
