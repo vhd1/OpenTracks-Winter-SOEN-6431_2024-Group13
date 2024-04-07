a670a18e3264d335e4cee784bc0ba4307572c47b
Avi Lad <avilad1010@gmail.com>
2024-04-07 14:26:24 -0400

    rilling/OpenTracks-Winter-SOEN-6431_2024#60 Adding checks in addTrackpoint() in TrackStatisticsUpdater to check if it is waiting for chairlift and updates its values in TrackStatistics object

--- src/main/java/de/dennisguse/opentracks/stats/TrackStatisticsUpdater.java
+++ src/main/java/de/dennisguse/opentracks/stats/TrackStatisticsUpdater.java
@@ -109,6 +109,13 @@ public class TrackStatisticsUpdater {
             currentSegment.addTotalAltitudeLoss(trackPoint.getAltitudeLoss());
         }
 
+        // this function will always be called for all trackpoints to check if it is waiting for chairlift
+        // and also modify values for the check according to current trackpoint.
+        if (isWaitingForChairlift(trackPoint)){
+            Duration passedDuration = Duration.between(lastTrackPoint.getTime(), trackPoint.getTime());
+            currentSegment.setTotalChairliftWaitingTime(currentSegment.getTotalChairliftWaitingTime().plus(passedDuration));
+        }
+
         //Update absolute (GPS-based) altitude
         if (trackPoint.hasAltitude()) {
             currentSegment.updateAltitudeExtremities(trackPoint.getAltitude());
@@ -167,6 +174,37 @@ public class TrackStatisticsUpdater {
         }
 
         lastTrackPoint = trackPoint;
+    }
+
+    /**
+     * This function can be used to check if current trackpoint is of type waiting for chairlift.
+     * It returns true if it is waiting at lower end of the track for more than 5 trackpoints.
+     * else it increments the counter if it is still at lower end of track.
+     * */
+    private boolean isWaitingForChairlift(TrackPoint trackPoint) {
+        // indicates altitude difference allowed in case of small change elevation change while waiting in queue for chairlift
+        final float minimumAltitudeChangeAllowed = 0.2f;
+        final float minimumDeviationAllowedFromLowestAltitude = 1f;
+        final int thresholdTrackpointsForNoMovement=5;
+
+        float altitudeGain = trackPoint.hasAltitudeGain()? trackPoint.getAltitudeGain(): 0f;
+        float altitudeLoss = trackPoint.hasAltitudeLoss()? trackPoint.getAltitudeLoss(): 0f;
+        float currentAltitudeChange = Math.max(altitudeGain, altitudeLoss);
+
+
+        if (currentAltitudeChange<=minimumAltitudeChangeAllowed
+                && trackPoint.getAltitude()!=null
+                && Math.abs(currentSegment.getMinAltitude()-trackPoint.getAltitude().toM())<=minimumDeviationAllowedFromLowestAltitude){
+            currentSegment.incrementEndOfRunCounter();
+            if (currentSegment.getEndOfRunCounter()>=thresholdTrackpointsForNoMovement){
+                return true;
+            }
+        } else {
+            currentSegment.resetEndOfRunCounter();
+            return false;
+        }
+
+        return false;
     }
 
     private void reset(TrackPoint trackPoint) {
@@ -232,4 +270,3 @@ public class TrackStatisticsUpdater {
                 '}';
     }
 }
- 
