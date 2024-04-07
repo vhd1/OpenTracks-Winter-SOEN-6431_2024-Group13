931e2416935796127ea86a3994401128f8210897
gup-abhi <agabhishek20121998@gmail.com>
2024-04-07 18:20:40 -0400

    Added discard logic

--- src/main/java/de/dennisguse/opentracks/settings/DefaultsSettingsFragment.java
+++ src/main/java/de/dennisguse/opentracks/settings/DefaultsSettingsFragment.java
@@ -7,15 +7,24 @@ import androidx.fragment.app.DialogFragment;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 import de.dennisguse.opentracks.R;
 import de.dennisguse.opentracks.data.models.ActivityType;
 import de.dennisguse.opentracks.fragments.ChooseActivityTypeDialogFragment;
+import android.util.Log;
+import android.preference.PreferenceManager;
+
+
 
 public class DefaultsSettingsFragment extends PreferenceFragmentCompat implements ChooseActivityTypeDialogFragment.ChooseActivityTypeCaller {
 
     // Used to forward update from ChooseActivityTypeDialogFragment; TODO Could be replaced with LiveData.
     private ActivityTypePreference.ActivityPreferenceDialog activityPreferenceDialog;
+    private SwitchPreferenceCompat autoDiscardSwitch;
+    private ListPreference recordLengthList;
+
+    private int minDuration = 5;
 
     private final SharedPreferences.OnSharedPreferenceChangeListener sharedPreferenceChangeListener = (sharedPreferences, key) -> {
         if (PreferencesUtils.isKey(R.string.stats_units_key, key)) {
@@ -26,6 +35,33 @@ public class DefaultsSettingsFragment extends PreferenceFragmentCompat implement
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings_defaults);
+        autoDiscardSwitch = findPreference(getString(R.string.auto_discard_key));
+        recordLengthList = findPreference(getString(R.string.record_length_options));
+
+        autoDiscardSwitch.setOnPreferenceChangeListener((preference, newValue) -> {
+            boolean autoDiscardEnabled = (boolean) newValue;
+            recordLengthList.setEnabled(autoDiscardEnabled);
+            Log.d("DefaultsSettingsFragment", "Auto-Discard preference changed: " + autoDiscardEnabled);
+
+            // Save the new value to SharedPreferences
+            updateAutoDiscardSwitch(autoDiscardEnabled);
+
+            return true;
+        });
+
+        recordLengthList.setOnPreferenceChangeListener((preference, newValue) -> {
+            int selectedLength = Integer.parseInt((String) newValue);
+            minDuration = selectedLength;
+            Log.d("DefaultsSettingsFragment", "minDuration: " + minDuration);
+            Log.d("DefaultsSettingsFragment", "Record Length preference changed: " + selectedLength + " seconds");
+
+            // Save the new value to SharedPreferences
+            updateRecordLengthList(selectedLength);
+
+            return true;
+        });
+
+
     }
 
     @Override
@@ -74,7 +110,7 @@ public class DefaultsSettingsFragment extends PreferenceFragmentCompat implement
             case IMPERIAL_FEET, IMPERIAL_METER ->
                     R.array.stats_rate_imperial_options;
             case NAUTICAL_IMPERIAL ->
-                R.array.stats_rate_nautical_options;
+                    R.array.stats_rate_nautical_options;
         };
 
         String[] entries = getResources().getStringArray(entriesId);
@@ -89,4 +125,19 @@ public class DefaultsSettingsFragment extends PreferenceFragmentCompat implement
             activityPreferenceDialog.updateUI(activityType);
         }
     }
+
+    // Method to update autoDiscardSwitch preference
+    private void updateAutoDiscardSwitch(boolean autoDiscardEnabled) {
+        PreferencesUtils.setBoolean(R.string.auto_discard_key, autoDiscardEnabled);
+    }
+
+    // Method to update recordLengthList preference
+    private void updateRecordLengthList(int selectedLength) {
+        PreferencesUtils.setString(R.string.record_length_default, String.valueOf(selectedLength));
+    }
+
+    // Method to check if an exercise record should be discarded based on its duration
+
+
+
 }
