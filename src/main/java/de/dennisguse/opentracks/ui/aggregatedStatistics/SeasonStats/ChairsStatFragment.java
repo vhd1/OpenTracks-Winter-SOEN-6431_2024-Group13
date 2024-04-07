cec711bd0ae342adb9e20d6536d2e098f20c0f2b
deBauch_Casanova <sujithkumar2003k@gmail.com>
2024-04-07 19:54:11 -0400

    Total # of days chair lift used feature #92

--- src/main/java/de/dennisguse/opentracks/ui/aggregatedStatistics/SeasonStats/ChairsStatFragment.java
+++ src/main/java/de/dennisguse/opentracks/ui/aggregatedStatistics/SeasonStats/ChairsStatFragment.java
@@ -1,13 +1,12 @@
 package de.dennisguse.opentracks.ui.aggregatedStatistics.SeasonStats;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import de.dennisguse.opentracks.R;
 
@@ -18,81 +17,54 @@ import de.dennisguse.opentracks.R;
  */
 public class ChairsStatFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
     private static final String ARG_SEASON_NAME = "seasonName";
-    private static final String ARG_SEASON_days = "days";
-    private static final String ARG_SEASON_tallestChair = "tallestChair";
+    private static final String ARG_SEASON_DAYS = "days";
+    private static final String ARG_SEASON_TALLEST_CHAIR = "tallestChair";
+    private static final String ARG_SEASON_TOTAL_DAYS_CHAIRLIFT_USED = "totalNumberOfDaysChairliftUsed";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private static String mSeasonName;
-    private static String mDays;
-    private static String mTallestChair;
+
+    private String mSeasonName;
+    private String mDays;
+    private String mTallestChair;
+    private String mTotalDaysChairliftUsed;
 
     public ChairsStatFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChairsStatFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChairsStatFragment newInstance(String param1, String param2) {
+    public static ChairsStatFragment newInstance(String seasonName, String days, String tallestChair, String totalDaysChairliftUsed) {
         ChairsStatFragment fragment = new ChairsStatFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        args.putString(ARG_SEASON_NAME, mSeasonName);
-        args.putString(ARG_SEASON_days, mDays);
-        args.putString(ARG_SEASON_tallestChair, mTallestChair);
+        args.putString(ARG_SEASON_NAME, seasonName);
+        args.putString(ARG_SEASON_DAYS, days);
+        args.putString(ARG_SEASON_TALLEST_CHAIR, tallestChair);
+        args.putString(ARG_SEASON_TOTAL_DAYS_CHAIRLIFT_USED, totalDaysChairliftUsed);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_chairs_stat, container, false);
-        Bundle args = getArguments();
-        if (args != null) {
-            mSeasonName = args.getString(ARG_SEASON_NAME);
-            mDays = args.getString(ARG_SEASON_days);
-            mTallestChair = args.getString(ARG_SEASON_tallestChair);
+        if (getArguments() != null) {
+            mSeasonName = getArguments().getString(ARG_SEASON_NAME);
+            mDays = getArguments().getString(ARG_SEASON_DAYS);
+            mTallestChair = getArguments().getString(ARG_SEASON_TALLEST_CHAIR);
+            mTotalDaysChairliftUsed = getArguments().getString(ARG_SEASON_TOTAL_DAYS_CHAIRLIFT_USED);
         }
 
-        TextView seasonName = view.findViewById(R.id.seasonNameTextView);
-        TextView days = view.findViewById(R.id.days_tv);
-        TextView tallestChair = view.findViewById(R.id.tallestChair_tv);
+        TextView seasonNameTextView = view.findViewById(R.id.seasonNameTextView);
+        TextView daysTextView = view.findViewById(R.id.days_tv);
+        TextView tallestChairTextView = view.findViewById(R.id.tallestChair_tv);
+        TextView totalDaysChairliftUsedTextView = view.findViewById(R.id.totalDaysChairliftUsed_tv);
 
         if (mSeasonName != null) {
-            seasonName.setText(mSeasonName);
-            days.setText(mDays);
-            tallestChair.setText(mTallestChair);
-
+            seasonNameTextView.setText(mSeasonName);
+            daysTextView.setText(mDays);
+            tallestChairTextView.setText(mTallestChair);
+            totalDaysChairliftUsedTextView.setText(mTotalDaysChairliftUsed);
         }
         return view;
-    }
-
-    public void setArguments(Bundle args) {
-        this.setArguments(args);
     }
 }
--- src/main/java/de/dennisguse/opentracks/ui/aggregatedStatistics/SeasonStats/RunAndChairStatActivity.java
+++ src/main/java/de/dennisguse/opentracks/ui/aggregatedStatistics/SeasonStats/RunAndChairStatActivity.java
@@ -58,6 +58,7 @@ public class RunAndChairStatActivity extends AppCompatActivity {
             String max_alt="123";
             String tallestRun="123";
             String longestRun="123";
+            String totalNumberOfDaysChairliftUsed = "123";
             if (intent != null){
 
                 seasonName = intent.getStringExtra("seasonName");
@@ -71,10 +72,11 @@ public class RunAndChairStatActivity extends AppCompatActivity {
                 distance = Integer.toString((int)intent.getDoubleExtra("distance", -1.0));
                 max_Speed = Integer.toString((int)intent.getDoubleExtra("max_Speed", -1.0));
                 longestRun = Integer.toString((int)intent.getDoubleExtra("longestRun", -1.0));
+                totalNumberOfDaysChairliftUsed = Integer.toString((int)intent.getDoubleExtra("totalNumberOfDaysChairliftUsed",-1));
 
             }
             // Pass the string as an argument if the fragment is RunsStatFragment
-            fragmentTransaction.replace(R.id.frame_nav_season, ((RunsStatFragment) fragment).newInstance(seasonName, days, runs, resort, vertical_m, distance, active, max_Speed, max_alt, tallestRun, longestRun));
+            fragmentTransaction.replace(R.id.frame_nav_season, RunsStatFragment.newInstance(seasonName, days, runs, resort, vertical_m, distance, active, max_Speed, max_alt, tallestRun, longestRun), totalNumberOfDaysChairliftUsed);
         } else {
             fragmentTransaction.replace(R.id.frame_nav_season, fragment);
         }
