b6fcb02feaf27662c3d778abb7170d5851280703
rextivlan <affan.ali368@gmail.com>
2024-04-07 20:42:18 -0400

    Add attributes and constructor for ETS Updater

--- src/main/java/de/dennisguse/opentracks/stats/EnhancedTrackStatisticsUpdater.java
+++ src/main/java/de/dennisguse/opentracks/stats/EnhancedTrackStatisticsUpdater.java
@@ -9,6 +9,12 @@ import de.dennisguse.opentracks.data.models.TrackPoint;
 import de.dennisguse.opentracks.stats.TrackStatistics;
 import de.dennisguse.opentracks.stats.TrackStatisticsUpdater;
 
-public class EnhancedTrackStatisticsUpdater {
-    
+public class EnhancedTrackStatisticsUpdater extends TrackStatisticsUpdater {
+    private static final double CHAIRLIFT_SPEED_THRESHOLD = 1.5; // meters/second
+    private static final double ALTITUDE_CHANGE_FOR_CHAIRLIFT = 10.0; // meters
+
+    public EnhancedTrackStatisticsUpdater() {
+        super();
+    }
+
 }
