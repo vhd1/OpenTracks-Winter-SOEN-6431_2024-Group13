1f899569518bfac4c57775e03743e1f6e56e3ac0
rextivlan <affan.ali368@gmail.com>
2024-04-07 20:40:40 -0400

    Add Enhanced Track Statistics Updater

---
+++ src/main/java/de/dennisguse/opentracks/stats/EnhancedTrackStatisticsUpdater.java
@@ -0,0 +1,14 @@
+package de.dennisguse.opentracks.stats;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
+import de.dennisguse.opentracks.data.models.Speed;
+import de.dennisguse.opentracks.data.models.TrackPoint;
+import de.dennisguse.opentracks.stats.TrackStatistics;
+import de.dennisguse.opentracks.stats.TrackStatisticsUpdater;
+
+public class EnhancedTrackStatisticsUpdater {
+    
+}
