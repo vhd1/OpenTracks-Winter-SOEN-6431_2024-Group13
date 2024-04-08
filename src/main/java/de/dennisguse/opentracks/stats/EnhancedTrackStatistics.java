e5beaeef6587b13feedb3fd475939d66f217c74c
rextivlan <affan.ali368@gmail.com>
2024-04-07 20:35:14 -0400

    Add Attributes and Constructor for Enhanced Track Statistics

--- src/main/java/de/dennisguse/opentracks/stats/EnhancedTrackStatistics.java
+++ src/main/java/de/dennisguse/opentracks/stats/EnhancedTrackStatistics.java
@@ -10,5 +10,19 @@ import de.dennisguse.opentracks.stats.TrackStatistics;
 import de.dennisguse.opentracks.stats.TrackStatisticsUpdater;
 
 public class EnhancedTrackStatistics extends TrackStatistics {
-    
+    private double averageSlope; // In percentage
+    private double topSpeed; // In meters/second
+    private Speed averageSpeed; // In meters/second
+    private long timeOnChairlift; // In milliseconds
+    private long waitingTimeForChairlift; // In milliseconds
+
+    // Constructor
+    public EnhancedTrackStatistics() {
+        super();
+        this.averageSlope = 0.0;
+        this.topSpeed = 0.0;
+        this.averageSpeed = Speed.of(0);
+        this.timeOnChairlift = 0L;
+        this.waitingTimeForChairlift = 0L;
+    }
 }
